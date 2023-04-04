f7a251e82ac931bc7b43fc60f5fa16a526518b95
JeongHoon Lim <hoony6134@gmail.com>
2023-04-04 20:02:19 +0900

    commit

--- 1277.c
+++ 1277.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 int main(){
-    int N;
-    
+    int N,cnt,temp;
+    int f,m,l;
+    cnt=0;
+    scanf("%d",&N);
+    while(cnt<=N){
+        cnt++;
+        scanf("%d",&temp);
+        if(cnt==1){
+            f=temp;
+        }
+        if(N/2 + 1 == cnt){
+            m=temp;
+        }
+        if(N==cnt){
+            l=temp;
+        }
+    }
 }
